cec1d71b5cf026440d313db5f4aafa9dd9bad64a
Alix VEYRAT <alixveyrat@gmail.com>
2025-02-12 11:44:24 +0100

    dernier commit

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 // Étape 1
-// Créer une fonction qui demande un nombre à l’utilisateur à l’aide 
-// d’un prompteur. (Attention tous les compilateurs en ligne 
-// ne permettent pas la prise d’input, prendre celui dans l’énoncé si besoin).
-// Stocker sa réponse dans une variable de type adéquat nommée givenNumber.
 
-    let givenNumber = prompt("Coucou petite perruche! Devine un nombre entre 1 et 30")
+function choixNombre(){
+    let givenNumber=Number(prompt("Coucou petite perruche! Devine un nombre entre 1 et 30"))
+    return givenNumber
+}
+let givenNumber = choixNombre()
 
-// Etape 2
-// Pour le moment nous allons considérer que le nombre à deviner est 22.
+console.log(givenNumber)
+    // Étape 2
 
-// Écrire une fonction qui prend en paramètre givenNumber et 
-// qui se nommera didIWin
+function didIWin(givenNumber){
+    if (givenNumber < 22){
+        alert("C'est plus !")}
+    else if (givenNumber > 22){
+        alert("C'est moins !")} 
+    else {
+        alert("Bravo le veau! Tu as deviné le nombre")
+    }
+}
+didIWin(givenNumber)
 
-// Si le nombre est plus petit que 22, on affichera à l’utilisateur “Plus grand”.
-// Si le nombre est plus quand que 22 on affichera à l’utilisateur “Plus petit”.
-// Si le nombre est 22 on affichera “Bravo ! Vous avez deviné le nombre”.
-// L’ordre dans lequel vous ferez vos conditions est important, 
-// le but est d’exécuter le moins de code possible. 
-// Demandez-vous ce qu’il faut vérifier en premier.
+//    // Étape 3
+// function gamePlay(didIWin, givenNumber){
+//     if (didIWin(givenNumber) === true){
+//         alert("Tu veux rejouer?")
+//     } else {
+//         for (;;) {
+//             monPrompt()
+//             didIWin(givenNumber)
+//             if (givenNumber===22) {
+//             break ;
+//             console.log(givenNumber);
+//             }
+//     }
+//     }
+// }
 
-// ⚠️ Attention : le prompt() bloque l’exécution de la page 
-// et stoppe l’affichage du rendu. Pour afficher des informations à la suite 
-// d’un prompt(), utilisez alert().
-
-// Créer une fonction gamePlay qui gérera vos appels de fonctions 
-// et la transmission de votre variable d’une fonction à une autre.
+   
+        
+// gamePlay(didIWin,givenNumber)
